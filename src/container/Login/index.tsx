acf60c34ebc2796e4c621b6e1ffb6e83ab8aac01
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-08-31 16:10:09 +0800

    feat:渐入文字

--- src/container/Login/index.tsx
+++ src/container/Login/index.tsx
@@ -1,21 +1,43 @@
-import { useCanvasBreathingEffect } from '@/hook/useCanvasBreathingEffect'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap'
 
 import styles from './styles.module.less'
 import logo from '@/assets/logo.svg'
+import { useCanvasBreathingEffect } from '@/hook/useCanvasBreathingEffect'
 
 const Login = () => {
+  const textRef = useRef<HTMLSpanElement>(null)
   const canvasRef = useCanvasBreathingEffect()
+  const text = '小账童'
+
+  useEffect(() => {
+    const char = textRef.current?.querySelectorAll('span')
+    if (!char) return
+    gsap.from(char, {
+      opacity: 0,
+      duration: 1,
+      ease: 'bounce.out',
+      stagger: 0.2,
+    })
+  })
 
   return (
-    <>
+    <div className={styles.login}>
       <canvas ref={canvasRef} className={styles.canvas_bg}></canvas>
       <div className={styles.login_header}>
         <div className={styles.login_logo}>
           <img src={logo} alt='login' />
         </div>
-        <div>小账童</div>
+        <div className={styles.login_title}>
+          <span ref={textRef} className={styles.login_title_text}>
+            {text.split('').map((char, i) => (
+              <span key={i}>{char}</span>
+            ))}
+          </span>
+          <div className={styles.placeholder} />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
