3e0afc846704539364e0fc3e5e80f36a65fe6378
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-13 21:48:55 +0800

    feat(AllBills): 添加账单页面头部布局和样式
    添加账单页面头部标题、月份选择及收支明细展示区域

--- src/container/AllBills/index.tsx
+++ src/container/AllBills/index.tsx
@@ -1,7 +1,44 @@
 import styles from './styles.module.less'
+import { Divider } from 'antd-mobile'
 
 const AllBills = () => {
-  return <div className={styles.commonBackground}>AllBills</div>
+  return (
+    <div className={styles.commonBackground}>
+      <div className={styles.header}>
+        <span className={styles.header_title}>账单</span>
+        <div className={styles.header_partculars}>
+          <div className={styles.header_partculars_date}>
+            <span>09</span>
+            <span>月</span>
+          </div>
+
+          <Divider />
+
+          <div className={styles.header_partculars_item}>
+            <div>
+              <span>收入</span>
+              <span>2640.00</span>
+            </div>
+
+            <div>
+              <span>支出</span>
+              <span>2640.00</span>
+            </div>
+
+            <div>
+              <span>结余</span>
+              <span>2640.00</span>
+            </div>
+
+            <div>
+              <span>待还</span>
+              <span>2640.00</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AllBills
