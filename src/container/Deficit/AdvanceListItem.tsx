b6a19b857f31f39397581bb793464b53a9b2a63e
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-13 20:32:13 +0800

    feat(Deficit): 添加预支占比显示和排行榜功能
    添加预支占比进度条及其颜色动态变化功能，并显示预支排行榜
    新增警告提示当赤字超过20%时显示

---
+++ src/container/Deficit/AdvanceListItem.tsx
@@ -0,0 +1,6 @@
+import styles from './styles.module.less'
+
+const AdvanceListItem = () => {
+  return <div className={styles.item}>1231231</div>
+}
+export default AdvanceListItem
--- src/container/Deficit/index.tsx
+++ src/container/Deficit/index.tsx
@@ -1,13 +1,52 @@
+import { useEffect, useState } from 'react'
+import { ProgressBar } from 'antd-mobile'
 import cs from 'classnames'
 
 import styles from './styles.module.less'
 import PieChart from './PieChart'
+import AdvanceListItem from './AdvanceListItem'
 
 const Deficit = () => {
+  const [percent, setPercent] = useState<number>(0)
+  const [progressColor, setProgressColor] =
+    useState<string>('hsl(0, 100%, 50%)')
+
+  // 预支占比 后期用api代替
+  const advance = 20
+
+  useEffect(() => {
+    setPercent(advance)
+    // 根据advance调整颜色，advance越高，红色越少，渐变为橙色或绿色
+    const hue = Math.min(120, (advance / 100) * 120) // 控制色相，0度为红色，120度为绿色
+    setProgressColor(`hsl(${hue}, 100%, 50%)`)
+  }, [advance])
+
+  // 以上全是假数据
+
   return (
     <div className={cs(styles.commonBackground, styles.deficit)}>
       <div className={styles.header}>
         <PieChart />
+        <div className={styles.PreAllocationRatio}>
+          <span>预支占比 : {advance}%</span>
+          <ProgressBar
+            style={{ '--fill-color': progressColor, '--track-width': '11px' }}
+            percent={percent}
+            className={styles.PreAllocationRatio_progress}
+          />
+        </div>
+      </div>
+
+      <div className={styles.message}>
+        <span>⚠️ 警告：赤字已经超过收入的20%，这属于高风险行为</span>
+      </div>
+
+      <div className={styles.content}>
+        <span className={styles.content_title}>预支排行榜</span>
+
+        <div className={styles.content_list}>
+          <AdvanceListItem />
+        </div>
       </div>
     </div>
   )
