c3536bfad7b3a780691599a36b35f1cf6e3f6a4f
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-13 22:03:23 +0800

    style(AllBills): 优化账单页面样式布局
    - 添加AllBills容器样式，设置内边距
    - 调整header布局和样式，包括标题、日期和财务信息展示
    - 使用classnames合并className
    - 优化财务信息项的文本样式和间距

--- src/container/AllBills/index.tsx
+++ src/container/AllBills/index.tsx
@@ -1,9 +1,10 @@
+import cs from 'classnames'
 import styles from './styles.module.less'
 import { Divider } from 'antd-mobile'
 
 const AllBills = () => {
   return (
-    <div className={styles.commonBackground}>
+    <div className={cs(styles.commonBackground, styles.AllBills)}>
       <div className={styles.header}>
         <span className={styles.header_title}>账单</span>
         <div className={styles.header_partculars}>
@@ -12,25 +13,30 @@ const AllBills = () => {
             <span>月</span>
           </div>
 
-          <Divider />
+          <Divider
+            direction='vertical'
+            style={{
+              height: 30,
+            }}
+          />
 
           <div className={styles.header_partculars_item}>
-            <div>
+            <div className={styles.header_partculars_item_text}>
               <span>收入</span>
               <span>2640.00</span>
             </div>
 
-            <div>
+            <div className={styles.header_partculars_item_text}>
               <span>支出</span>
               <span>2640.00</span>
             </div>
 
-            <div>
+            <div className={styles.header_partculars_item_text}>
               <span>结余</span>
               <span>2640.00</span>
             </div>
 
-            <div>
+            <div className={styles.header_partculars_item_text}>
               <span>待还</span>
               <span>2640.00</span>
             </div>
