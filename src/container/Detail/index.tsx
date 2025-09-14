17fd1b4375926bc76feaf276575ed71396025881
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-14 21:59:38 +0800

    feat(Detail): 添加餐饮列表样式和功能
    - 新增列表内容区域样式和滚动功能
    - 添加餐饮图标和列表项布局
    - 调整整体页面padding和文字颜色

--- src/container/Detail/index.tsx
+++ src/container/Detail/index.tsx
@@ -1,5 +1,6 @@
 import cs from 'classnames'
 import { AiFillCaretDown } from 'react-icons/ai'
+import { FaUtensils } from 'react-icons/fa6'
 
 import styles from './index.module.less'
 import logo from '@/assets/svg/logo.svg'
@@ -53,16 +54,56 @@ const Detail = () => {
         </div>
       </div>
 
-      <div className={styles.list}>
-        <div className={styles.list_title}>
-          <div className={styles.list_title_date}>
-            <span>09月14日</span>
-            <span>星期日</span>
+      <div className={styles.content}>
+        <div className={styles.list}>
+          <div className={styles.list_title}>
+            <div className={styles.list_title_date}>
+              <span>09月14日</span>
+              <span>星期日</span>
+            </div>
+            <div className={styles.list_title_money}>支出 : 73.00</div>
           </div>
-          <div className={styles.list_title_money}>支出 : 73.00</div>
+
+          <div className={styles.list_content}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => {
+              return (
+                <div className={styles.list_item} key={item}>
+                  <div className={styles.list_item_icon}>
+                    <FaUtensils />
+                    <span>餐饮</span>
+                  </div>
+                  <div className={styles.list_item_money}>
+                    <span>-73.00</span>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
-        <div className={styles.list_item}>
-          <div className={styles.list_item_icon}>支出</div>
+        <div className={styles.list}>
+          <div className={styles.list_title}>
+            <div className={styles.list_title_date}>
+              <span>09月14日</span>
+              <span>星期日</span>
+            </div>
+            <div className={styles.list_title_money}>支出 : 73.00</div>
+          </div>
+
+          <div className={styles.list_content}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => {
+              return (
+                <div className={styles.list_item} key={item}>
+                  <div className={styles.list_item_icon}>
+                    <FaUtensils />
+                    <span>餐饮</span>
+                  </div>
+                  <div className={styles.list_item_money}>
+                    <span>-73.00</span>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
