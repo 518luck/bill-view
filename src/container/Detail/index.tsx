8497e98c122c3823806d70860fdf414c0911d30a
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-14 19:24:18 +0800

    feat(Detail): 添加详情页头部布局和样式
    为详情页添加头部布局，包含logo和标题，并设置页面内边距

--- src/container/Detail/index.tsx
+++ src/container/Detail/index.tsx
@@ -1,7 +1,19 @@
+import cs from 'classnames'
+
 import styles from './index.module.less'
+import logo from '@/assets/svg/logo.svg'
 
 const Detail = () => {
-  return <div className={styles.commonBackground}>详情</div>
+  return (
+    <div className={cs(styles.commonBackground, styles.Detail)}>
+      <div className={styles.header}>
+        <div className={styles.headerIcon}>
+          <img src={logo} alt='AI' />
+        </div>
+        <div>小账童</div>
+      </div>
+    </div>
+  )
 }
 
 export default Detail
