90c09eb134fd94c5f3d2a4a97b42d5a33c304ed1
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-14 21:29:34 +0800

    feat(Detail): 添加消息卡片组件展示收支数据
    添加消息卡片组件样式和逻辑，包含日期显示和收支数据展示。使用flex布局实现响应式设计，并添加图标增强用户体验。

--- src/container/Detail/index.tsx
+++ src/container/Detail/index.tsx
@@ -1,14 +1,69 @@
 import cs from 'classnames'
+import { AiFillCaretDown } from 'react-icons/ai'
 
 import styles from './index.module.less'
 import logo from '@/assets/svg/logo.svg'
 
 const Detail = () => {
+  const messageData = [
+    {
+      title: '收入',
+      value: '25540.00',
+    },
+    {
+      title: '支出',
+      value: '15540.00',
+    },
+    {
+      title: '预期',
+      value: '10000.00',
+    },
+  ]
   return (
     <div className={cs(styles.commonBackground, styles.Detail)}>
       <div className={styles.header}>
         <img src={logo} alt='AI' className={styles.header_icon} />
         <div className={styles.header_title}>小账童</div>
+      </div>
+
+      <div className={styles.message}>
+        <div className={styles.message_date}>
+          <div className={styles.message_date_year}>2025年</div>
+          <div className={styles.message_date_month}>
+            <span>09</span>
+            <span>月</span>
+            <AiFillCaretDown />
+          </div>
+        </div>
+        <div className={styles.message_divider} />
+
+        <div className={styles.message_content}>
+          {messageData.map((item) => {
+            return (
+              <div className={styles.message_content_item} key={item.title}>
+                <div className={styles.message_content_item_title}>
+                  {item.title}
+                </div>
+                <div className={styles.message_content_item_value}>
+                  {item.value}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
+      <div className={styles.list}>
+        <div className={styles.list_title}>
+          <div className={styles.list_title_date}>
+            <span>09月14日</span>
+            <span>星期日</span>
+          </div>
+          <div className={styles.list_title_money}>支出 : 73.00</div>
+        </div>
+        <div className={styles.list_item}>
+          <div className={styles.list_item_icon}>支出</div>
+        </div>
       </div>
     </div>
   )
