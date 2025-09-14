bcedc4968dcbbe1effb206ab2b5e6c8ece42d754
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-15 00:28:39 +0800

    feat(Tally): 添加新的Tally组件
    添加Tally组件及相关样式文件，用于实现新的功能模块

---
+++ src/container/Tally/index.tsx
@@ -0,0 +1,9 @@
+import cs from 'classnames'
+
+import styles from './index.module.less'
+
+const Tally = () => {
+  return <div className={cs(styles.commonBackground, styles.tally)}></div>
+}
+
+export default Tally
