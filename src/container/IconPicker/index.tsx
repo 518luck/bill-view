25fb06664fb74aaa65ce7f875fe30b0bb1c482a6
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-10-12 17:06:54 +0800

    feat(IconPicker): 添加返回按钮和头部导航栏
    在 IconPicker 组件中添加了返回按钮和头部导航栏，使用 react-icons 和 antd-mobile 的 Space 组件实现更好的用户体验

--- src/container/IconPicker/index.tsx
+++ src/container/IconPicker/index.tsx
@@ -2,11 +2,22 @@ import cs from 'classnames'
 import styles from './styles.module.less'
 import { useNavigate } from 'react-router-dom'
 import BillTypeTabs from '@/components/BillTypeTabs'
+import { MdArrowBackIosNew } from 'react-icons/md'
+import { Space } from 'antd-mobile'
 
 const IconPicker = () => {
   const navigate = useNavigate()
   return (
     <div className={cs(styles.commonBackground, styles.icon_picker)}>
+      <div className={styles.header}>
+        <Space
+          onClick={() => navigate('/tally')}
+          justify='start'
+          align='center'>
+          <MdArrowBackIosNew size={15} />
+          返回
+        </Space>
+      </div>
       <span onClick={() => navigate('/tally')}>返回</span>
       <div>
         tab栏
