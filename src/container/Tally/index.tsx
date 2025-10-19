94f215f7ce99690337902bf773357a37fa7fffe3
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-10-19 13:31:05 +0800

    feat(Tally): 实现动态图标列表和分类切换功能
    添加动态图标列表功能，根据当前账单类型切换显示对应的图标
    使用DynamicIcon组件替换静态图标，从API获取图标数据
    添加账单类型切换的回调函数，更新当前类型状态

--- src/container/Tally/index.tsx
+++ src/container/Tally/index.tsx
@@ -1,20 +1,28 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import cs from 'classnames'
-import { FaUtensils } from 'react-icons/fa'
 import { MdSettings } from 'react-icons/md'
 
 import styles from './styles.module.less'
 import Keypad from './Keypad'
 import BillTypeTabs from '@/components/BillTypeTabs'
+import DynamicIcon from '@/components/DynamicIcon'
+import { useGetIconList } from '@/api'
 
 const Tally = () => {
   const navigate = useNavigate()
+  const [currentTabsType, setCurrentTabsType] = useState<'expense' | 'income'>(
+    'expense'
+  )
+  const { data: iconList } = useGetIconList({
+    type: currentTabsType,
+  })
 
   return (
     <div className={cs(styles.commonBackground, styles.tally)}>
       <div className={styles.header}>
         <div className={styles.tab}>
-          <BillTypeTabs size='medium' />
+          <BillTypeTabs size='medium' onChange={setCurrentTabsType} />
         </div>
         <div className={styles.cancel}>
           <MdSettings size={24} onClick={() => navigate('/iconPicker')} />
@@ -23,11 +31,11 @@ const Tally = () => {
 
       <div className={styles.content}>
         <div className={styles.content_list}>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item) => {
+          {iconList?.map((item) => {
             return (
-              <div className={styles.content_item} key={item}>
-                <FaUtensils size={18} />
-                <span>餐饮</span>
+              <div className={styles.content_item} key={item.id}>
+                <DynamicIcon name={item.icon_name} size={18} />
+                <span>{item.title}</span>
               </div>
             )
           })}
