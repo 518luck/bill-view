21e25965228bd8e431b669ef256c0075099a04f8
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-10-27 23:25:16 +0800

    feat(Debts): 添加预付款表单字段
    为预付款表单新增本月应还金额、预计还清日期、已还金额、欠款开始日期和欠款总额度字段，完善表单功能

--- src/container/Debts/AddPrepayment.tsx
+++ src/container/Debts/AddPrepayment.tsx
@@ -32,12 +32,77 @@ const AddPrepayment = ({ visible, setVisible }: AddPrepaymentProps) => {
       onClose={() => {
         setVisible(false)
       }}>
-      <Space>
-        <Text size='large'>欠款方</Text>
+      <Space align='center'>
+        <Text size='large'>欠款方：</Text>
         <Controller
           name='creditor'
           control={control}
           render={({ field }) => <ZInput {...field} placeholder='欠款方' />}
+        />
+      </Space>
+      <Space align='center'>
+        <Text size='large'>本月应还金额：</Text>
+        <Controller
+          name='current_month_due'
+          control={control}
+          render={({ field }) => (
+            <ZInput
+              {...field}
+              value={field.value?.toString() ?? ''}
+              onChange={(val) => field.onChange(Number(val))}
+              placeholder='本月应还金额'
+            />
+          )}
+        />
+      </Space>
+      <Space align='center'>
+        <Text size='large'>预计还清日期：</Text>
+        <Controller
+          name='end_date'
+          control={control}
+          render={({ field }) => (
+            <ZInput {...field} placeholder='预计还清日期' />
+          )}
+        />
+      </Space>
+      <Space align='center'>
+        <Text size='large'>已还金额：</Text>
+        <Controller
+          name='repaid_amount'
+          control={control}
+          render={({ field }) => (
+            <ZInput
+              {...field}
+              value={field.value?.toString() ?? ''}
+              onChange={(val) => field.onChange(Number(val))}
+              placeholder='已还金额'
+            />
+          )}
+        />
+      </Space>
+      <Space align='center'>
+        <Text size='large'>欠款开始日期</Text>
+        <Controller
+          name='start_date'
+          control={control}
+          render={({ field }) => (
+            <ZInput {...field} placeholder='欠款开始日期' />
+          )}
+        />
+      </Space>
+      <Space align='center'>
+        <Text size='large'>欠款总额度</Text>
+        <Controller
+          name='total_amount'
+          control={control}
+          render={({ field }) => (
+            <ZInput
+              {...field}
+              value={field.value?.toString() ?? ''}
+              onChange={(val) => field.onChange(Number(val))}
+              placeholder='欠款方'
+            />
+          )}
         />
       </Space>
     </ZPopup>
